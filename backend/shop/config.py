d532c446ac83453f33a5fafbf3b7ebc5c307ce14
Freyzan2006 <122388366+Freyzan2006@users.noreply.github.com>
2023-12-09 17:46:07 +0500

    Add files via upload

---
+++ backend/shop/config.py
@@ -0,0 +1,8 @@
+SECRET_KEY = "secret key"
+UPLOAD_FOLDER = './shop/static/uploads/'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///shop.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+URL_BACKAND = "http://127.0.0.1:5000"
+URL_IMAGES = "./static/uploads/"
